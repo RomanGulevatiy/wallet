1cecc04ca62521996ccbe9880d1098dba60382aa
RomanGulevatiy <gulevatiy.roman@gmail.com>
2026-04-26 17:27:02 +0300

    feat: add application properties for Telegram bot and update test configuration

--- src/main/java/com/duikt/wallet/config/BotConfig.java
+++ src/main/java/com/duikt/wallet/config/BotConfig.java
@@ -3,15 +3,11 @@ package com.duikt.wallet.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
-@Configuration
-@PropertySource("classpath:application.properties")
 public class BotConfig {
 
     @Value("${telegram.bot.username}")
